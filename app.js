46c765da565888ad5014c0879baa11040042d025
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-02-02 13:16:41 +0200

    start

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var cookie = require('cookie');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.listen(8800, function () {
+  console.log('Example app listening on port 8800!');
+});
+
+
+
+
